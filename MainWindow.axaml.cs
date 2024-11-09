8c9f3066494fa811f00b7249ec7e2c6a30feb72a
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2024-11-09 21:17:18 +0800

    Initial commit

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AvaloniaFontWeightIssue;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
